285c757bf18e36782478318291790f5d9fe762f4
funkinaoki <funkinaoki@gmail.com>
2022-05-04 17:43:57 +0900

    fading

--- src/gatsby-theme-mate/sections/Landing.js
+++ src/gatsby-theme-mate/sections/Landing.js
@@ -8,6 +8,7 @@ import Triangle from 'gatsby-theme-mate/src/components/Triangle';
 import { useSiteQuery } from 'gatsby-theme-mate/src/queries/useSiteQuery';
 import { SECTION } from '../utils/constants';
 import { getSectionHref } from 'gatsby-theme-mate/src/utils/helpers';
+import { Fade } from 'react-awesome-reveal';
 
 
 const centerHorizontally = { marginRight: 'auto', marginLeft: 'auto' };
@@ -17,41 +18,45 @@ const LandingPage = () => {
 
   return (
     <Section.Container id={SECTION.home} Background={Background}>
-      <Heading
-        textAlign="center"
-        as="h1"
-        color="primary"
-        fontSize={[5, 7]}
-        mb={[3, 4, 5]}
-      >
-        {`${name}`}
-      </Heading>
-
-      <Heading
-        as="h2"
-        color="primary"
-        fontSize={[4, 6]}
-        mb={[3, 5]}
-        textAlign="center"
-        style={centerHorizontally}
-      >
-        <TextLoop interval={5000}>
-          {roles
-            .sort(() => (deterministic ? 1 : Math.random() - 0.5))
-            .map((text) => (
-              <Text width={[300, 500]} key={text}>
-                {text}
-              </Text>
-            ))}
-        </TextLoop>
-      </Heading>
-
+      <Fade direction="down" triggerOnce>
+        <Heading
+          textAlign="center"
+          as="h1"
+          color="primary"
+          fontSize={[5, 7]}
+          mb={[3, 4, 5]}
+        >
+          {name}
+        </Heading>
+      </Fade>
+      <Fade direction="down" triggerOnce>
+        <Heading
+          as="h2"
+          color="primary"
+          fontSize={[4, 6]}
+          mb={[3, 5]}
+          textAlign="center"
+          style={centerHorizontally}
+        >
+          <TextLoop interval={5000}>
+            {roles
+              .sort(() => (deterministic ? 1 : Math.random() - 0.5))
+              .map((text) => (
+                <Text width={[300, 500]} key={text}>
+                  {text}
+                </Text>
+              ))}
+          </TextLoop>
+        </Heading>
+      </Fade>
       <Flex alignItems="center" justifyContent="center" flexWrap="wrap">
-        {socialLinks.map((sl) => (
-          <Box mx={3} fontSize={[5, 6, 6]} key={sl.name}>
-            <SocialLink {...sl} />
-          </Box>
-        ))}
+        <Fade direction="right" triggerOnce cascade damping={0.5}>
+          {socialLinks.map((sl) => (
+            <Box mx={3} fontSize={[5, 6, 6]} key={sl.name}>
+              <SocialLink {...sl} />
+            </Box>
+          ))}
+        </Fade>
       </Flex>
 
       <ScrollIcon href={`#${getSectionHref(SECTION.about)}`} />
@@ -61,30 +66,33 @@ const LandingPage = () => {
 
 const Background = () => (
   <>
+  <Fade direction="left" triggerOnce>
     <Triangle
       color="muted"
       height={['35vh', '80vh']}
       width={['95vw', '60vw']}
     />
-
+  </Fade>
+  <Fade direction="left" triggerOnce>
     <Triangle
       color="secondary"
       height={['38vh', '80vh']}
       width={['50vw', '35vw']}
     />
-
+  </Fade>
+  <Fade direction="right" triggerOnce>
     <Triangle
       color="primary"
       height={['25vh', '35vh']}
       width={['75vw', '60vw']}
       position="top-right"
     />
-
+  </Fade>
     <Triangle
       color="muted"
       height={['20vh', '20vh']}
       width={['100vw', '100vw']}
-      position="bottom-right"
+      position="bottom-left"
     />
   </>
 );
