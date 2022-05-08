ce3b005802e03be4b95503d0121663b54392989e
Kazuki Taninaka <onelive555@gmail.com>
2022-05-08 12:09:16 +0900

    update: threshold

--- src/gatsby-theme-mate/hooks/useScroll.ts
+++ src/gatsby-theme-mate/hooks/useScroll.ts
@@ -2,13 +2,13 @@ import { useState, useEffect } from "react";
 
 const useScroll = () => {
   const [stage, setStage] = useState<0 | 1 | 2>(0);
-  const windowHeight = window.innerHeight;
+  const threshold = window.innerHeight / 2; // window.innerHeightの半分の値分スクロールしたらコンテンツを入れ替える
 
   const handleScroll = () => {
-    const scrollAmountFromContents = window.pageYOffset - windowHeight;
-    if (scrollAmountFromContents < windowHeight) {
+    const scrollAmountFromContents = window.pageYOffset - window.innerHeight;
+    if (scrollAmountFromContents < threshold) {
       setStage(0);
-    } else if (scrollAmountFromContents < windowHeight * 2) {
+    } else if (scrollAmountFromContents < threshold * 2) {
       setStage(1);
     } else {
       setStage(2);
