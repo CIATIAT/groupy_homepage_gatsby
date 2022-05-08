752b49103e11f973b628a35e5df8cb86f2006fd0
Kazuki Taninaka <onelive555@gmail.com>
2022-05-08 18:03:13 +0900

    update: Cardのスタイル変更

---
+++ src/gatsby-theme-mate/components/Card.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import { Card as CardRebass } from "rebass/styled-components";
+
+type CardContainerProps = {
+  minWidth: string;
+};
+
+export const CardContainer = styled.div<CardContainerProps>`
+  display: grid;
+  grid-gap: 30px;
+
+  grid-template-columns: repeat(auto-fill, minmax(${({ minWidth }) => minWidth}, 1fr));
+  justify-items: center;
+
+  @media only screen and (max-width: 400px) {
+    grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+  }
+`;
+
+// NOTE: SPのカルーセルの場合、このスタイルが余計でなのでmedia query追加
+export const Card = styled(CardRebass).attrs({
+  bg: "background",
+  boxShadow: 0
+})`
+  position: relative;
+  box-shadow: 0 3px 3px rgba(0, 0, 0, 0.2);
+  transition: all 0.25s;
+  top: 0;
+  height: 100%;
+  border-radius: 8px;
+
+  @media (min-width: 768px) {
+    &:hover {
+      top: -10px;
+      box-shadow: 0 12px 16px rgba(0, 0, 0, 0.2);
+    }
+  }
+  @media (max-width: 768px) {
+    margin: 0 5px;
+  }
+`;
