11438bc7825930c3f3da1d0dc057b9903dd3c7a0
Kazuki Taninaka <onelive555@gmail.com>
2022-05-15 20:51:00 +0900

    refactor: way to consume useHelmetQuery

--- src/gatsby-theme-mate/components/Helmet.tsx
+++ src/gatsby-theme-mate/components/Helmet.tsx
@@ -9,7 +9,7 @@ type Props = {
 };
 
 const Helmet = ({ theme }: Props) => {
-  const { description, profile } = useHelmetQuery();
+  const query = useHelmetQuery();
   const title = `Groupy株式会社`;
   const domain = `groupy.space`;
 
@@ -17,28 +17,28 @@ const Helmet = ({ theme }: Props) => {
     <ReactHelmet htmlAttributes={{ lang: "jp" }}>
       <meta charSet="utf-8" />
       <title>Groupy Inc.</title>
-      <meta name="description" content={description} />
-      <link rel="shortcut icon" href={`https:${domain}${profile.favicon32.src}`} />
-      <meta name="theme-color" content={"#000000"} data-react-helmet="true"/>
+      <meta name="description" content={query.description} />
+      <link rel="shortcut icon" href={`https:${domain}${query.profile.favicon32.src}`} />
+      <meta name="theme-color" content={"#000000"} data-react-helmet="true" />
       <meta name="apple-mobile-web-app-status-bar-style" content={theme.colors.groupyPurple} />
-      <meta name="image" content={`https:${domain}${profile.favicon32.src}`} />
+      <meta name="image" content={`https:${domain}${query.profile.favicon32.src}`} />
       <meta itemProp="name" content={title} />
-      <meta itemProp="description" content={description} />
-      <meta itemProp="image" content={`https:${domain}${profile.favicon32.src}`} />
+      <meta itemProp="description" content={query.description} />
+      <meta itemProp="image" content={`https:${domain}${query.profile.favicon32.src}`} />
       <meta name="og:title" content={title} />
-      <meta name="og:description" content={description} />
-      <meta name="og:image" content={`https:${domain}${profile.bigIcon.src}`} />
+      <meta name="og:description" content={query.description} />
+      <meta name="og:image" content={`https:${domain}${query.profile.bigIcon.src}`} />
       <meta name="og:site_name" content={title} />
       <meta name="og:locale" content="ja" />
       <meta name="og:type" content="website" />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={`https:${domain}${profile.bigIcon.src}`} />
-      <meta name="twitter:image:src" content={`https:${domain}${profile.bigIcon.src}`} />
-      <link rel="apple-touch-icon" sizes="180x180" href={`https:${domain}${profile.appleIcon.src}`} />
-      <link rel="icon" type="image/png" sizes="32x32" href={`https:${domain}${profile.favicon32.src}`} />
-      <link rel="icon" type="image/png" sizes="16x16" href={`https:${domain}${profile.favicon16.src}`} />
+      <meta name="twitter:description" content={query.description} />
+      <meta name="twitter:image" content={`https:${domain}${query.profile.bigIcon.src}`} />
+      <meta name="twitter:image:src" content={`https:${domain}${query.profile.bigIcon.src}`} />
+      <link rel="apple-touch-icon" sizes="180x180" href={`https:${domain}${query.profile.appleIcon.src}`} />
+      <link rel="icon" type="image/png" sizes="32x32" href={`https:${domain}${query.profile.favicon32.src}`} />
+      <link rel="icon" type="image/png" sizes="16x16" href={`https:${domain}${query.profile.favicon16.src}`} />
     </ReactHelmet>
   );
 };
