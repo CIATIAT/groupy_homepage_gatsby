dffafa006c46e2e29451c9976ad293da6abacfb3
Kazuki Taninaka <onelive555@gmail.com>
2022-05-28 17:57:58 +0900

    refactor: replace logo img with GatsbyImage from gatsby-plugin-image

--- src/gatsby-theme-mate/sections/About.tsx
+++ src/gatsby-theme-mate/sections/About.tsx
@@ -1,22 +1,18 @@
 import React from "react";
-import { Box, Image, Flex } from "rebass/styled-components";
-import styled from "styled-components";
+import { Box, Flex } from "rebass/styled-components";
 import ReactMarkdown from "react-markdown";
 import { Fade } from "react-awesome-reveal";
 import Section from "gatsby-theme-mate/src/components/Section";
 import components from "../components/MarkdownComponent";
-import { useAboutMeQuery } from "gatsby-theme-mate/src/queries/useAboutMeQuery";
 import { SECTION } from "../utils/constants";
 import { graphql, useStaticQuery } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 const About = () => {
-  const { markdown, profile } = useAboutMeQuery();
   const data = useStaticQuery(graphql`
     query AboutGroupyQuery {
       contentfulAsset(id: { eq: "96b7693b-b570-5091-8082-31695910fda3" }) {
-        resize(quality: 100, width: 450) {
-          src
-        }
+        gatsbyImageData(quality: 100, width: 450)
       }
       contentfulAbout {
         childContentfulAboutAboutMeTextNode {
@@ -27,6 +23,7 @@ const About = () => {
       }
     }
   `);
+  const groupyLogo = getImage(data.contentfulAsset);
 
   return (
     <Section.Container id={SECTION.about}>
@@ -42,21 +39,12 @@ const About = () => {
         </Box>
         <Box width={[1, 1, 2 / 6]} style={{ maxWidth: "300px", margin: "auto" }}>
           <Fade triggerOnce>
-            <ProfilePicture mt={[0, 4, 0]} ml={[0, 0, 1]} {...data.contentfulAsset.resize} />
+            <GatsbyImage image={groupyLogo} alt="groupyのロゴ画像" />
           </Fade>
         </Box>
       </Flex>
     </Section.Container>
   );
 };
-
-const ProfilePicture = styled(Image)`
-  border-radius: 50%;
-  transition: all 0.4s ease-out;
-
-  &:hover {
-    border-radius: 20%;
-  }
-`;
 
 export default About;
