a79f72446f5db70103f6465e0ff557f67991bd0e
Kazuki Taninaka <onelive555@gmail.com>
2022-06-06 22:50:31 +0900

    chore: install gatsby-plugin-breakpoints

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,22 +2,23 @@ const { ACCESS_TOKEN, SPACE_ID, ANALYTICS_ID } = process.env;
 
 const plugins = [
   {
-    resolve: `gatsby-theme-mate`,
+    resolve: "gatsby-theme-mate",
     options: {
       accessToken: ACCESS_TOKEN,
       spaceId: SPACE_ID
     }
   },
-  `gatsby-plugin-image`,
-  `gatsby-transformer-sharp`,
+  "gatsby-plugin-image",
+  "gatsby-transformer-sharp",
   {
-    resolve: `gatsby-source-filesystem`,
+    resolve: "gatsby-source-filesystem",
     options: {
-      name: `images`,
+      name: "images",
       path: `${__dirname}/src/images`
     }
   },
-  `gatsby-plugin-sharp`
+  "gatsby-plugin-sharp",
+  "gatsby-plugin-breakpoints"
 ];
 
 if (ANALYTICS_ID) {
