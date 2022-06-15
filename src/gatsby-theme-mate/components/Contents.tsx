bc3889e49e84e1f0ce0584e2b61d1cae5ef0490e
Kazuki Taninaka <onelive555@gmail.com>
2022-06-15 12:40:04 +0900

    refactor: move useScroll intto Context.tsx

--- src/gatsby-theme-mate/components/Contents.tsx
+++ src/gatsby-theme-mate/components/Contents.tsx
@@ -1,23 +1,51 @@
-import React from "react";
-import { Box } from "rebass/styled-components";
-import useScroll from "../../hooks/useScroll";
+import { useEffect, useState } from "react";
+import { Box, SxStyleProp } from "rebass/styled-components";
+import { isBrowser } from "../../utils";
 import About from "../sections/About";
 import Member from "../sections/Member";
 import News from "../sections/News";
 // import BG from "/sampleBG.jpeg";
 
-const Contents = () => {
-  const stage = useScroll();
-  const renderContents = () => {
-    switch (stage) {
-      case "about":
-        return <About />;
-      case "member":
-        return <Member />;
-      case "news":
-        return <News />;
+const useStage = () => {
+  const [stage, setStage] = useState<"about" | "member" | "news">("about");
+
+  const handleScroll = () => {
+    if (!isBrowser()) return;
+    const threshold = window.innerHeight;
+    const scrollAmountFromContents = window.pageYOffset - window.innerHeight;
+
+    if (scrollAmountFromContents < threshold) {
+      setStage("about");
+    } else if (scrollAmountFromContents < threshold * 2) {
+      setStage("member");
+    } else {
+      setStage("news");
     }
   };
+
+  useEffect(() => {
+    if (!isBrowser()) return;
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return stage;
+};
+
+const Contents = () => {
+  const stage = useStage();
+
+  const generateConditionalStyles = (stageName: typeof stage): SxStyleProp => {
+    return {
+      visibility: stage === stageName ? "visible" : "hidden",
+      height: stage === stageName ? "auto" : 0,
+      opacity: stage === stageName ? 1 : 0,
+      transition: "opacity 0.3s"
+    };
+  };
+
   return (
     <>
       <Box
@@ -32,7 +60,17 @@ const Contents = () => {
           // backgroundSize: "cover"
         }}
       >
-        <Box sx={{ marginBottom: "200px" }}>{renderContents()}</Box>
+        <Box sx={{ marginBottom: "200px" }}>
+          <Box sx={generateConditionalStyles("about")}>
+            <About />
+          </Box>
+          <Box sx={generateConditionalStyles("member")}>
+            <Member />
+          </Box>
+          <Box sx={generateConditionalStyles("news")}>
+            <News />
+          </Box>
+        </Box>
       </Box>
       <Box sx={{ height: "300vh" }}></Box>
     </>
